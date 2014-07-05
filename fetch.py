d5408beb054fc01ed1a98eb29813cf7612f6355e
Glenn Bitar <glennbitar@gmail.com>
2014-07-05 13:46:47 +0000

    Come a long way on GY63 python code.

--- fetch.py
+++ fetch.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from smbus import SMBus
 import time
 
@@ -10,43 +11,83 @@ CMD_READ_T = 0x58
 CMD_GET = 0x00
 CMD_PROM_PRES_SENS = 0xA2
 CMD_PROM_PRES_OFFS = 0xA4
+CMD_PROM_TEMP_COEF_PRES_SENS = 0xA6
+CMD_PROM_TEMP_COEF_PRES_OFFS = 0xA8
+CMD_PROM_TEMP_REF = 0xAA
+CMD_PROM_TEMP_COEF = 0xAC
 
-# make gy63 read pressure
-bus.write_byte(ADR, CMD_READ_P)
-time.sleep(0.05)
+presSens = 0
+presOffs = 0
+tempCoefSens = 0
+tempCoefOffs = 0
+tempRef = 0
+tempCoef = 0
 
-# get pressure
-adcPresRead = bus.read_i2c_block_data(ADR, CMD_GET)
-time.sleep(0.05)
+temp = 0
+pres = 0
 
-# make gy63 read temp
-bus.write_byte(ADR, CMD_READ_T)
-time.sleep(0.05)
+def initGY63():
+	presSens = bus.read_word_data(ADR, CMD_PROM_PRES_SENS)
+	time.sleep(0.05)
 
-# get temp
-adcTempRead = bus.read_i2c_block_data(ADR, CMD_GET)
-time.sleep(0.05)
+	presOffs = bus.read_word_data(ADR, CMD_PROM_PRES_OFFS)
+	time.sleep(0.05)
 
-# calculate raw pressure data
-rawPres = adcPresRead[0] * 0x10000 + adcPresRead[1] * 0x100 + adcPresRead[2]
+	tempCoefSens = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF_PRES_SENS)
+	time.sleep(0.05)
 
-# calculate raw temperature data
-rawTemp = adcTempRead[0] * 0x10000 + adcTempRead[1] * 0x100 + adcTempRead[2]
+	tempCoefOffs = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF_PRES_OFFS)
+	time.sleep(0.05)
 
-# get factory data
-presSensRead = bus.read_i2c_block_data(ADR, CMD_PROM_PRES_SENS)
-time.sleep(0.05)
+	tempRef = bus.read_word_data(ADR, CMD_PROM_TEMP_REF)
+	time.sleep(0.05)
 
-presOffsRead = bus.read_i2c_block_data(ADR, CMD_PROM_PRES_OFFS)
-time.sleep(0.05)
+	tempCoef = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF)
+	time.sleep(0.05)
+	
+	print "Initialization complete."
+	print "presSens", presSens
+	print "presOffs", presOffs
+	print "tempCoefSens", tempCoefSens
+	print "tempCoefOffs", tempCoefOffs
+	print "tempRef", tempRef
+	print "tempCoef", tempCoef
 
-tempCoefPresSensRead = bus.read_i2c_block_data(ADR, CMD_PROM_TEMP_COEF_PRES_SENS)
+def getGY63():
+	# make gy63 read pressure
+	bus.write_byte(ADR, CMD_READ_P)
+	time.sleep(0.05)
+	
+	# get pressure
+	adcPresRead = bus.read_i2c_block_data(ADR, CMD_GET)
+	time.sleep(0.05)
 
-tempCoefPresOffsRead = bus.read_i2c_block_data(ADR, CMD_PROM_TEMP_COEF_PRES_OFFS)
+	# calculate raw pressure data
+	rawPres = adcPresRead[0] * 0x10000 + adcPresRead[1] * 0x100 + adcPresRead[2]
 
-tempRefRead = bus.read_i2c_block_data(ADR, CMD_TEMP_REF_READ)
+	# make gy63 read temp
+	bus.write_byte(ADR, CMD_READ_T)
+	time.sleep(0.05)
 
-presSens = presSensRead[0] * 256.0 + presSensRead[1]
-presOffs = presOffsRead[0] * 256.0 + presOffsRead[1]
+	# get temp
+	adcTempRead = bus.read_i2c_block_data(ADR, CMD_GET)
+	time.sleep(0.05)
 
-print adcRead
+	# calculate raw temperature data
+	rawTemp = adcTempRead[0] * 0x10000 + adcTempRead[1] * 0x100 + adcTempRead[2]
+
+	dt = rawTemp - tempRef * 2**8
+	temp = 2000.0 + dt * tempCoef / 2**23
+	off = presOffs * 2**16 + tempCoefOffs * dt / 2**7
+	sens = presSens * 2**15 + tempCoefSens * dt / 2**8
+	pres = (presSens * sens / 2**21 - off) / 2**15
+
+	print dt, pres, temp
+
+initGY63()
+
+while True:
+	getGY63()
+	#print pres, temp
+	time.sleep(2)
+
