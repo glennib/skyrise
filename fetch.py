ea445785cfef5f3b640e1a2cf7b90ce66482a5b7
Glenn Bitar <glennbitar@gmail.com>
2014-07-05 14:50:30 +0000

    Updated code. Now gets some data but it's not sensible. ~820 mbar pressure and ~140 c temperature does not seem right.

--- fetch.py
+++ fetch.py
@@ -16,44 +16,49 @@ CMD_PROM_TEMP_COEF_PRES_OFFS = 0xA8
 CMD_PROM_TEMP_REF = 0xAA
 CMD_PROM_TEMP_COEF = 0xAC
 
-presSens = 0
-presOffs = 0
-tempCoefSens = 0
-tempCoefOffs = 0
-tempRef = 0
-tempCoef = 0
+c1 = 0
+c2 = 0
+c3 = 0
+c4 = 0
+c5 = 0
+c6 = 0
 
 temp = 0
 pres = 0
 
 def initGY63():
-	presSens = bus.read_word_data(ADR, CMD_PROM_PRES_SENS)
+	global c1, c2, c3, c4, c5, c6
+	
+	c1 = bus.read_word_data(ADR, 0xa2)
 	time.sleep(0.05)
 
-	presOffs = bus.read_word_data(ADR, CMD_PROM_PRES_OFFS)
+	c2 = bus.read_word_data(ADR, 0xa4)
 	time.sleep(0.05)
 
-	tempCoefSens = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF_PRES_SENS)
+	c3 = bus.read_word_data(ADR, 0xa6)
 	time.sleep(0.05)
 
-	tempCoefOffs = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF_PRES_OFFS)
+	c4 = bus.read_word_data(ADR, 0xa8)
 	time.sleep(0.05)
 
-	tempRef = bus.read_word_data(ADR, CMD_PROM_TEMP_REF)
+	c5 = bus.read_word_data(ADR, 0xaa)
 	time.sleep(0.05)
 
-	tempCoef = bus.read_word_data(ADR, CMD_PROM_TEMP_COEF)
+	c6 = bus.read_word_data(ADR, 0xac)
 	time.sleep(0.05)
 	
 	print "Initialization complete."
-	print "presSens", presSens
-	print "presOffs", presOffs
-	print "tempCoefSens", tempCoefSens
-	print "tempCoefOffs", tempCoefOffs
-	print "tempRef", tempRef
-	print "tempCoef", tempCoef
+	print "presSens", c1
+	print "presOffs", c2
+	print "tempCoefSens", c3
+	print "tempCoefOffs", c4
+	print "tempRef", c5
+	print "tempCoef", c6
+	print ""
 
 def getGY63():
+	global temp, pres
+
 	# make gy63 read pressure
 	bus.write_byte(ADR, CMD_READ_P)
 	time.sleep(0.05)
@@ -76,18 +81,19 @@ def getGY63():
 	# calculate raw temperature data
 	rawTemp = adcTempRead[0] * 0x10000 + adcTempRead[1] * 0x100 + adcTempRead[2]
 
-	dt = rawTemp - tempRef * 2**8
-	temp = 2000.0 + dt * tempCoef / 2**23
-	off = presOffs * 2**16 + tempCoefOffs * dt / 2**7
-	sens = presSens * 2**15 + tempCoefSens * dt / 2**8
-	pres = (presSens * sens / 2**21 - off) / 2**15
+	dT = rawTemp - c5 * 2**8
+	off = c2 * 2**17 + dT * c4 / 2**6
+	sens = c1 * 2**16 + dT * c3 / 2**7
 
-	print dt, pres, temp
+	temp = (2000 + dT * c6 / 2**23) / 100
+	pres = (rawPres * sens / 2**21 - off) / 2**15 / 100
 
 initGY63()
 
 while True:
 	getGY63()
-	#print pres, temp
+	print "T:", temp
+	print "P:", pres
+	print ""
 	time.sleep(2)
 
