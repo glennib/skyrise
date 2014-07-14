e7bfb6264602f644081650a6f5cc19923816ff1b
Glenn Bitar <glennbitar@gmail.com>
2014-07-15 00:32:40 +0200

    Added comments

--- gps.py
+++ gps.py
@@ -4,9 +4,12 @@ import serial
 lat = 0
 lon = 0
 alt = 0
-good = False
+good = False # This contains whether the last GPS read contained good information.
+
+# Each one of the process functions is to parse the different types of strings that come from the chip.
 
 def processRMC(inp):
+	# We use the third ([2]) entry of RMC to determine if the data coming from the GPS chip is legit.
 	if inp[2] == 'V':
 		good = False
 	elif inp[2] == 'A':
@@ -18,28 +21,29 @@ def processVTG(inp):
 	return
 
 def processGGA(inp):
-	global lat, lon, alt
+	global lat, lon, alt # Grants access to the global variables.
 
-	buf = inp[2][:2]
-	lat = float(buf)
-	buf = inp[2][2:]
-	lat += float(buf) / 60
-	if inp[3] == 'S':
+	buf = inp[2][:2] # The two first digits of the latitude is degrees.
+	lat = float(buf) # Turn it into a number.
+	buf = inp[2][2:] # The rest is minutes (with decimals)
+	lat += float(buf) / 60 # Add it to the degrees.
+	if inp[3] == 'S': # Determine if it's south, if so, negate.
 		lat *= -1
-	elif inp[3] != 'N':
+	elif inp[3] != 'N': # If it's neither S nor N, then what??
 		print "ERROR: Neither north or south??"
 
-	buf = inp[4][:3]
-	lon = float(buf)
-	buf = inp[4][:3]
-	lon += float(buf) / 60
-	if inp[5] == 'W':
+	buf = inp[4][:3] # Grab longitude.
+	lon = float(buf) # Turn it into number.
+	buf = inp[4][:3] # Grab minutes.
+	lon += float(buf) / 60 # Add it to lon.
+	if inp[5] == 'W': # If it's west, negate.
 		lon *= -1
-	elif inp[5] != 'E'
+	elif inp[5] != 'E' # If it's not east nor west, then what?
 		print "ERROR: Neither east or west??"
 
-	alt = float(buf[9])
+	alt = float(buf[9]) # Grab the altitude and turn it into a number.
 
+	# This is for debugging purposes.
 	print "Lat:", lat
 	print "Lon:", lon
 	print "Alt:", alt
@@ -53,21 +57,24 @@ def processGSV(inp):
 def processGLL(inp):
 	return
 
+# This takes a finished line from the chip, and determines which function to parse it through.
 def processGPS(inp):
-	if inp == "":
+	if inp == "": # If the string input is empty, we don't do anything. This may happen on the first run.
 		return
-	elif inp[0] != '$':
+	elif inp[0] != '$': # If the string input doesn't start with a $, there's a parsing error. Don't do anything with it.
 		return
 
-	arr = inp.split(',')
+	arr = inp.split(',') # Here we split the input by commas, so we can easily handle each of the data elements. Element 0 will indicate the type of message.
 
+	# Here we read element 0 and pass arr to the right function.
 	if arr[0] == "$GPRMC":
 		processRMC(arr)
 	elif arr[0] == "$GPVTG":
 		processVTG(arr)
 	elif arr[0] == "$GPGGA":
-		if good:
+		if good: # The good variable must be True to parse the GGA-string.
 			processGGA(arr)
+			finishGPS() # after GGA is parsed, we have the info we need. finishGPS handles what happens after that.
 	elif arr[0] == "$GPGSA":
 		processGSA(arr)
 	elif arr[0] == "$GPGSV":
@@ -78,19 +85,18 @@ def processGPS(inp):
 		print "ERROR. Unknown message from GPS:", inp
 
 
-PORT = '/dev/ttyAMA0'
-REFRESH = 0.20
-BAUD = 9600
+PORT = '/dev/ttyAMA0' # Serial port to read GPS string from.
+BAUD = 9600 # Baud rate for the GPS chip.
 
-s = serial.Serial(PORT, BAUD, timeout=1)
+s = serial.Serial(PORT, BAUD, timeout=1) # Open serial port.
 
-time.sleep(2)
+time.sleep(0.5)
 
-curString = ""
+curString = "" # Contains the unfinished string.
 while(True):
-	c = s.read()
-	if c == '$':
+	c = s.read() # Reads next character.
+	if c == '$': # If the next character is a $, we have a new line to read. Take what we already have and parse it.
 		processGPS(curString)
-		curString = "$"
-	else:
+		curString = "$" # Restart curString.
+	else: # Add last read char to curString.
 		curString += c
