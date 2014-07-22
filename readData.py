64fd192c357b56e66c5cd357ce9fc71cb3b160a1
Glenn Bitar <glennbitar@gmail.com>
2014-07-22 11:42:01 +0200

    Added reading file

---
+++ readData.py
@@ -0,0 +1,79 @@
+import time
+
+DATAPATH = '/home/pi/skyrise/data/'
+ACCELEROMETER_FILE = 'accelerometer.txt'
+COMPASS_FILE = 'compass.txt'
+BAROMETER_FILE = 'pressure.txt'
+
+
+pressure = 0.0
+temperature = 0.0
+
+accX = ""
+accY = ""
+accZ = ""
+
+heading = ""
+
+def readBarometer():
+	global pressure, temperature
+	s = ""
+	try:
+		f = open(DATAPATH + BAROMETER_FILE, "r")
+		try:
+			s = f.read()
+		except:
+			pass
+		finally:
+			f.close()
+	except:
+		pass
+	li = s.rsplit(',')
+	pressure = li[0]
+	temperature = li[1]
+
+
+def readCompass():
+	global heading
+	s = ""
+	try:
+		f = open(DATAPATH + COMPASS_FILE, "r")
+		try:
+			s = f.read()
+		except:
+			pass
+		finally:
+			f.close()
+	except:
+		pass
+	heading = s
+
+def readAccelerometer():
+	global accX, accY, accZ
+	s = ""
+	try:
+		f = open(DATAPATH + ACCELEROMETER_FILE, "r")
+		try:
+			s = f.read()
+		except:
+			pass
+		finally:
+			f.close()
+	except:
+		pass
+	li = s.rsplit(',')
+	accX = li[0]
+	accY = li[1]
+	accZ = li[2]
+
+while True:
+	readBarometer()
+	readAccelerometer()
+	readCompass()
+	
+	print chr(27) + "[2J"
+	print "Pressure:", pressure
+	print "Temperature:", temperature
+	print "Heading:", heading
+	print "Acceleration (X, Y, Z): {0}, {1}, {2}".format(accX, accY, accZ)
+	time.sleep(0.01)
