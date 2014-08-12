d38663b68f21307d472f57062ea36f4284d0c014
krilor <krilor@gmail.com>
2014-08-12 08:42:56 +0200

    Added option to leave fields blank when pushing data

--- web/pushdata.php
+++ web/pushdata.php
@@ -4,7 +4,7 @@
 	To push data to the mysql database 
 	
 	Interface:
-	http://skyrise.hit.no/datahandle/pushdata.php?pass=simplepassword&time=1406803997&lat=60.000&lon=-2&alt=1002&pressure=500&tempin=12&tempout=-30&heading=-40&accelx=1.001&accely=0.4421&accelz=-0.51325&humidity=25&spin=5&voltage=5.6
+	http://skyrise.hit.no/wp-content/plugins/pst/pushdata.php?pass=simplepassword&time=1406803997&lat=60.000&lon=-2&alt=1002&pressure=500&tempin=12&tempout=-30&heading=-40&accelx=1.001&accely=0.4421&accelz=-0.51325&humidity=25&spin=5&voltage=5.6
 	
 */
 // Include config file
@@ -18,27 +18,29 @@ $dbhandle = mysql_connect($pstconfig['dbserver'], $pstconfig['dbuser'], $pstconf
 $selected = mysql_select_db($pstconfig['dbname'],$dbhandle) 
   or die("ERROR4");
 
-// Check that all variables exist
+// Check that the password exist and that is is correct
 if(isset($_GET["pass"])):
 	if( $_GET["pass"] == $pstconfig['dbpushpass'] ):
-		if(isset($_GET["time"],$_GET["lat"],$_GET["lon"],$_GET["alt"],$_GET["pressure"],$_GET["tempin"],$_GET["tempout"],$_GET["heading"],$_GET["accelx"],$_GET["accely"],$_GET["accelz"],$_GET["humidity"],$_GET["spin"],$_GET["voltage"])):
+
+		// If there is a timestamp, store whatevers there into the db
+		if(isset($_GET["time"])):
 		  
 			// Build sql query
 			$query = 	"INSERT INTO " . $pstconfig['dbtable'] . " VALUES ( DEFAULT, "
 						. "FROM_UNIXTIME('" . htmlspecialchars($_GET["time"]) . "'), "
-						. htmlspecialchars($_GET["lat"]) . ", "
-						. htmlspecialchars($_GET["lon"]) . ", "
-						. htmlspecialchars($_GET["alt"]) . ", "
-						. htmlspecialchars($_GET["pressure"]) . ", "
-						. htmlspecialchars($_GET["tempin"]) . ", "
-						. htmlspecialchars($_GET["tempout"]) . ", "
-						. htmlspecialchars($_GET["heading"]) . ", "
-						. htmlspecialchars($_GET["accelx"]) . ", "
-						. htmlspecialchars($_GET["accely"]) . ", "
-						. htmlspecialchars($_GET["accelz"]) . ", "
-						. htmlspecialchars($_GET["humidity"]). ", "
-						. htmlspecialchars($_GET["spin"]). ", "
-						. htmlspecialchars($_GET["voltage"]) . " ) ";
+						. (isset($_GET["lat"]) ? htmlspecialchars($_GET["lat"]) : 'DEFAULT') . ", "
+						. (isset($_GET["lon"]) ? htmlspecialchars($_GET["lon"]) : 'DEFAULT') . ", "
+						. (isset($_GET["alt"]) ? htmlspecialchars($_GET["alt"]) : 'DEFAULT') . ", "
+						. (isset($_GET["pressure"]) ? htmlspecialchars($_GET["pressure"]) : 'DEFAULT') . ", "
+						. (isset($_GET["tempin"]) ? htmlspecialchars($_GET["tempin"]) : 'DEFAULT') . ", "
+						. (isset($_GET["tempout"]) ? htmlspecialchars($_GET["tempout"]) : 'DEFAULT') . ", "
+						. (isset($_GET["heading"]) ? htmlspecialchars($_GET["heading"]) : 'DEFAULT') . ", "
+						. (isset($_GET["accelx"]) ? htmlspecialchars($_GET["accelx"]) : 'DEFAULT') . ", "
+						. (isset($_GET["accely"]) ? htmlspecialchars($_GET["accely"]) : 'DEFAULT') . ", "
+						. (isset($_GET["accelz"]) ? htmlspecialchars($_GET["accelz"]) : 'DEFAULT') . ", "
+						. (isset($_GET["humidity"]) ? htmlspecialchars($_GET["humidity"]) : 'DEFAULT'). ", "
+						. (isset($_GET["spin"]) ? htmlspecialchars($_GET["spin"]) : 'DEFAULT'). ", "
+						. (isset($_GET["voltage"]) ? htmlspecialchars($_GET["voltage"]) : 'DEFAULT') . " ) ";
 						
 			//execute the SQL query
 			$result = mysql_query($query);
@@ -51,7 +53,7 @@ if(isset($_GET["pass"])):
 			}
 		
 		else:
-			echo "ERROR1"; // Not all values
+			echo "ERROR1"; // Time is missing
 		endif;
 	else:
 		echo "ERROR2"; // Wrong pass
